fb63ff660df5cb5de6197a0b737f4814193fa3ec
Yaroslav Zviezda <acroreiser@gmail.com>
2026-05-08 00:11:39 +0300

    Revert "oom: support task's adj rbtree"
    This reverts commit 70d81ef8416868b1d9daf57c46e29d003f90a677.

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -58,11 +58,8 @@ void compare_swap_oom_score_adj(int old_val, int new_val)
 	struct sighand_struct *sighand = current->sighand;
 
 	spin_lock_irq(&sighand->siglock);
-	if (current->signal->oom_score_adj == old_val) {
-		delete_from_adj_tree(current);
+	if (current->signal->oom_score_adj == old_val)
 		current->signal->oom_score_adj = new_val;
-		add_2_adj_tree(current);
-	}
 	trace_oom_score_adj_update(current);
 	spin_unlock_irq(&sighand->siglock);
 }
@@ -81,10 +78,8 @@ int test_set_oom_score_adj(int new_val)
 	int old_val;
 
 	spin_lock_irq(&sighand->siglock);
-	delete_from_adj_tree(current);
 	old_val = current->signal->oom_score_adj;
 	current->signal->oom_score_adj = new_val;
-	add_2_adj_tree(current);
 	trace_oom_score_adj_update(current);
 	spin_unlock_irq(&sighand->siglock);
 
